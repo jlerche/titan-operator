5f94c3b6159c289e906de9c130582814b858554e
Jacob Lerche <jacob.lerche@gmail.com>
2019-01-31 16:29:10 -0800

    add more types

--- pkg/apis/titanclusterresource/v1/types.go
+++ pkg/apis/titanclusterresource/v1/types.go
@@ -12,6 +12,37 @@ type TitanCluster struct {
 	metav1.ObjectMeta `json:"metadata"`
 }
 
+type ContainerType string
+
+const (
+	PDContainerType ContainerType = "pd"
+	TitanContainerType ContainerType = "titan"
+	TiKVContainerType ContainerType = "tikv"
+	// TiDB has a Prometheus pushgateway definition here
+	UnknownContainerType ContainerType = "unknown"
+)
+
+func (ct ContainerType) String() string {
+	return string(ct)
+}
+
+type PDStatus struct {
+	StatefulSet PDStatefulSetStatus `json:"statefulSet,omitempty"`
+	Members map[string]PDMember `json:"members,omitempty"`
+	Upgrading bool `json:"upgrading,omitempty"`
+	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty"`
+}
+
+type PDStatefulSetStatus struct {
+	Name string `json:"name,omitempty"`
+}
+
+type PDMember struct {
+	Name string `json:"name"`
+	ID string `json:"id"`
+	IP string `json:"ip"`
+}
+
 type TitanStatus struct {
 	Members map[string]TitanMember `json:"members,omitempty"`
 	Upgrading bool `json:"upgrading,omitempty"`
